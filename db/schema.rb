c50ef34b9b80843cc9588b6d70808d1ca28fb0f7
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-06-23 19:43:00 +0000

    attempt to debug rake grade

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_23_183640) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_23_185338) do
   create_table "deliveries", force: :cascade do |t|
     t.string "description"
+    t.date "expected_date"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "supposed_to_arrive_on"
     t.integer "user_id"
-    t.boolean "arrived"
+    t.boolean "actual_arrive"
   end
 
   create_table "users", force: :cascade do |t|
